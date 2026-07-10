985c16eb8a07193e82400c0995dd74a982f67608
DaBigCheez3 <bosstin8@gmail.com>
2026-07-10 09:47:08 -0400

    quadratic hash

---
+++ src/HTQuadratic.cpp
@@ -0,0 +1,3 @@
+//
+// Created by bosst on 7/9/2026.
+//
--- src/HTQuadratic.h
+++ src/HTQuadratic.h
@@ -1,8 +1,43 @@
+#pragma once
+#include "Patient.h"
+
 //
 // Created by bosst on 7/9/2026.
 //
-
+using namespace std;
 #ifndef PROJECT1_HTQUADRATIC_H
 #define PROJECT1_HTQUADRATIC_H
+#include <string>
 
 #endif //PROJECT1_HTQUADRATIC_H
+
+class Entry {
+    public:
+    string id;
+    Patient patient;
+    bool occupied;
+    bool deleted;
+
+    Entry() {
+        occupied = false;
+        deleted = false;
+    }
+};
+
+
+
+class HTQuadratic {
+
+    private:
+    int size;
+    int capacity;
+    double lf;
+
+
+
+public:
+    HTQuadratic();
+
+
+
+};
