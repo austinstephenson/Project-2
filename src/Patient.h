6fc4dd5a90dfd5861b6dd004b97bd4ad91deb192
DaBigCheez3 <bosstin8@gmail.com>
2026-07-09 22:05:08 -0400

    PatientH
    Last Name First name added

--- src/Patient.h
+++ src/Patient.h
@@ -14,7 +14,8 @@ class Patient {
 private:
 
     string id;
-    string name;
+    string lastname;
+    string firstname;
     int age;
     char sex;
     double height; //inches
@@ -37,7 +38,8 @@ private:
 
     Patient(
     string id,
-    string name,
+    string lastname,
+    string firstname,
     int age,
     char sex,
     double height, //inches
